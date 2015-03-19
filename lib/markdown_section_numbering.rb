69a791a4989474b2d9d64198c45c7317259caac0
xoyip <xoyip@piyox.info>
2015-03-19 09:07:11 +0900

    add executable

--- lib/markdown_section_numbering.rb
+++ lib/markdown_section_numbering.rb
@@ -1,18 +1,16 @@
-require "markdown_section_numbering/version"
+require 'markdown_section_numbering/version'
 
 class MarkdownSectionNumbering
   class << self
-
     def config(max_level)
       @max_level = max_level
     end
 
     def convert(input)
       @section_index = [-1] + [0] * max_level
-      
       input.lines.map do |line|
         convert_line(line)
-      end.join("\n")+"\n"
+      end.join("\n") + "\n"
     end
 
   private
@@ -23,7 +21,8 @@ class MarkdownSectionNumbering
 
     def convert_line(line)
       line.chomp!
-      if not match = line.match(/^#+/)
+      match = line.match(/^#+/)
+      if !match
         line
       else
         level = match[0].length
@@ -33,23 +32,22 @@ class MarkdownSectionNumbering
 
     def add_section_number(line, target_level)
       return line if target_level > max_level
-      
-      header_sign = "#" * target_level
+
+      header_sign = '#' * target_level
       regex = Regexp.new("^#{header_sign}\\s*(\\d+(\\.\\d+)*)?\\s*(.+)")
       match = line.match(regex)
 
       number = calc_section_number(target_level)
-      
+
       "#{header_sign} #{number} #{match[3]}"
     end
 
     def calc_section_number(target_level)
       @section_index[target_level] += 1
-      (target_level+1 .. max_level).each do |child_level|
+      ((target_level + 1)..max_level).each do |child_level|
         @section_index[child_level] = 0
       end
-      (1..target_level).map {|level| @section_index[level].to_s }.join(".")
+      (1..target_level).map { |level| @section_index[level].to_s }.join('.')
     end
-    
   end
 end
--- markdown_section_numbering.gemspec
+++ markdown_section_numbering.gemspec
@@ -4,19 +4,20 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'markdown_section_numbering/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "markdown_section_numbering"
+  spec.name          = 'markdown_section_numbering'
   spec.version       = MarkdownSectionNumbering::VERSION
-  spec.authors       = ["xoyip"]
-  spec.email         = ["xoyip@piyox.info"]
-  spec.summary       = %q{Add section numbers to markdown headings.}
-  spec.description   = %q{}
-  spec.homepage      = "https://github.com/xoyip/markdown_section_numbering"
-  spec.license       = "MIT"
+  spec.authors       = ['xoyip']
+  spec.email         = ['xoyip@piyox.info']
+  spec.summary       = 'Add section numbers to markdown headings.'
+  spec.description   = ''
+  spec.homepage      = 'https://github.com/xoyip/markdown_section_numbering'
+  spec.license       = 'MIT'
 
+  spec.executables   = ['markdown-section-numbering']
   spec.files         = `git ls-files -z`.split("\x0")
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.6"
-  spec.add_development_dependency "rake"
+  spec.add_development_dependency 'bundler', '~> 1.6'
+  spec.add_development_dependency 'rake'
 end
